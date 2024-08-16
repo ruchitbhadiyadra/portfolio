b48be41a1de2b2b5f287238569ef4a66820c8c68
ruchitbhadiyadra <ruchitbhadiyadra@gmail.com>
2024-08-16 11:51:00 +0530

    portfolio

--- js/script.js
+++ js/script.js
@@ -117,3 +117,43 @@ $(document).ready(function () {
     });
 });
 
+// contact list fetch
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemeu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemeu.style.right = "0";
+}
+
+function closemenu() {
+    sidemeu.style.right = "-200px";
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbylY6v86tehQwL3AWrEVbMZr2l2r6KRrD5nO6Xd6BFVTi3cmtcVfPREWEpJ93F9w4iR/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message Sent Succesfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
